3466e6aa4c242ced643f6e3f5387143547263576
Baz <bk@thinkloop.com>
2016-09-21 21:29:46 +0000

    fixed position not setting for new todos

--- src/todos/actions/add-todo.js
+++ src/todos/actions/add-todo.js
@@ -4,7 +4,7 @@ import updateTodos from '../../todos/actions/update-todos';
 export default function (description) {
 	return (dispatch, getState) => {
 		const { todos } = getState();
-		const position = todos && todos.length || 0;
+		const position = Object.keys(todos || {}).length;
 
 		return newTodo(description, position).then(todo => {
 			const id = todo.id;
